9e39acdab7ca1e3c39cd62bb0a0024363e6479c8
Prasad Joshi <32058627+l3r4nd@users.noreply.github.com>
2017-12-12 13:06:42 +0530

    Create Re.split().py

---
+++ Regex and parsing/Re.split().py
@@ -0,0 +1,37 @@
+'''Problem statement:
+Input Format
+
+A single line of input containing the string .
+
+Output Format
+
+Print the numbers obtained after splitting the string on separate lines.
+
+Sample Input
+
+100,000,000.000
+
+Sample Output
+
+100
+000
+000
+000
+
+Note: Each line of output should only be the numbers. 
+'''
+
+## Solution:
+
+# import model
+import re
+# get numbers 
+numbers = input()
+#split on any character that is not a number
+print(*re.split('[\-=~!@#$%^&*()_+\[\]{};\'\\:"|<,./<>?]',numbers), sep = '\n')
+
+'''
+*: the '*' at the beginning removes '[]'
+[\-=~!@#$%^&*()_+\[\]{};\'\\:"|<,./<>?]: special characters defined to split on
+
+'''
